bb9cab3178034e5c95ae03281b1cb318eda22cde
tck <tck@dra100.com>
2020-12-08 20:36:25 +0800

    初始化文档

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # android屏幕录制
+
+[官方学习demo](https://github.com/android/media-samples/tree/792312519fc4e328594725b5d34c0a01f1de610d/ScreenCapture)
+
+https://github.com/yrom/ScreenRecorder
--- app/src/main/java/com/tck/screen/capture/MainActivity.kt
+++ app/src/main/java/com/tck/screen/capture/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.tck.screen.capture
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
+import com.tck.screen.capture.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnStartScreenCapture.setOnClickListener {
+
+
+        }
+    }
+
+
+    private fun initScreenCapture() {
+        val mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as? MediaProjectionManager
+        if (mediaProjectionManager==null){
+            return
+        }
+        val mediaProjection = mediaProjectionManager.getMediaProjection(100, intent)
+        val metrics = DisplayMetrics()
+
+//        mediaProjection.createVirtualDisplay(
+//                "ScreenCapture",
+//                binding.surfaceView.width,
+//                binding.surfaceView.height,
+//                display.getRealMetrics(metrics)
+//        )
+
+    }
+
+
+    private fun startScreenCapture(){
+
     }
 }
